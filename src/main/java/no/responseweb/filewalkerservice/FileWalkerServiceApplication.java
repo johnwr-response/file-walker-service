dfb6455ae4110897485d13a4b368aa9a6b2ffc93
johnwr-response <52628290+johnwr-response@users.noreply.github.com>
2020-06-12 16:20:10 +0200

    Initial commit

---
+++ src/main/java/no/responseweb/filewalkerservice/FileWalkerServiceApplication.java
@@ -0,0 +1,13 @@
+package no.responseweb.filewalkerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileWalkerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileWalkerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/no/responseweb/filewalkerservice/FileWalkerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package no.responseweb.filewalkerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileWalkerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
