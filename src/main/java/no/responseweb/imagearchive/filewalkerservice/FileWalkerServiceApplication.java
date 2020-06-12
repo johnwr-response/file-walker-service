a966e66121e6b1fdce59caca441e61a4b8929d11
johnwr-response <52628290+johnwr-response@users.noreply.github.com>
2020-06-12 16:36:12 +0200

    Moved package

--- src/main/java/no/responseweb/imagearchive/filewalkerservice/FileWalkerServiceApplication.java
+++ src/main/java/no/responseweb/imagearchive/filewalkerservice/FileWalkerServiceApplication.java
@@ -1,4 +1,4 @@
-package no.responseweb.filewalkerservice;
+package no.responseweb.imagearchive.filewalkerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/no/responseweb/imagearchive/filewalkerservice/FileWalkerServiceApplicationTests.java
+++ src/test/java/no/responseweb/imagearchive/filewalkerservice/FileWalkerServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package no.responseweb.filewalkerservice;
+package no.responseweb.imagearchive.filewalkerservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
